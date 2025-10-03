09d2b1cda2f12e7f8b51a5c0bf829ffaf0ecfaaa
Daruuu <thedaruny16@gmail.com>
2025-10-03 23:04:34 +0200

    start with methods of validation of cpp06/ex00

--- cpp_module_06/ex00/ScalarConverter.cpp
+++ cpp_module_06/ex00/ScalarConverter.cpp
@@ -1,1 +1,60 @@
 #include "ScalarConverter.hpp"
+
+#include <cstdlib>
+#include <cstring>
+
+ScalarConverter::ScalarConverter()
+{}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other)
+{
+	(void) other;
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
+{
+	(void) other;
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter() {}
+
+bool ScalarConverter::isChar(const std::string& literal)
+{
+	return (literal.length() == 1 && std::isprint(literal[0]) \
+		&& std::isalpha(literal[0]) && !std::isdigit(literal[0]));
+}
+
+bool ScalarConverter::isInteger(const std::string& literal)
+{
+	char *ptr = &literal;
+
+		// literal.at(0) == '-')
+	if (literal.length() < 1 && literal.max_size() > 11)
+	{
+		return false;
+	}
+	strtol(literal.c_str(), &ptr, 10);
+	//	42.
+	return (*ptr == '\0' && literal.find('.') != std::string::npos);
+}
+
+bool ScalarConverter::isDouble(const std::string& literal)
+{
+	char *ptr;
+	if (literal == "nanf" || literal == "+inff" || literal == "-inff")
+	{
+		return true;
+	}
+	strtof(literal.c_str(), &ptr);
+	// return (*ptr == 'f');
+	return (false);
+}
+
+bool ScalarConverter::isFloat(const std::string& literal)
+{
+}
+
+void ScalarConverter::convert(const std::string& literal)
+{
+}
--- cpp_module_06/ex00/ScalarConverter.hpp
+++ cpp_module_06/ex00/ScalarConverter.hpp
@@ -2,14 +2,35 @@
 #define SCALARCONVERTER_HPP
 
 #include <string>
+#include <limits.h>
 
 class ScalarConverter
 {
+
+private:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter &other);
+	ScalarConverter &operator=(const ScalarConverter &other);
+
 private:
 	static bool isChar(const std::string &literal);
+	static bool isInteger(const std::string &literal);
+	static bool isDouble(const std::string &literal);
+	static bool isFloat(const std::string &literal);
+
+	static bool convertToChar(const std::string &literal);
+	static bool convertToInteger(const std::string &literal);
+	static bool convertToDouble(const std::string &literal);
+	static bool convertToFloat(const std::string &literal);
+
+	void printChar(char valueCharacter);
+	void printInteger(int valueInteger);
+	void printDouble(double valueDouble);
+	void printFloat(float valueFloat);
 
 public:
-	static void convert(const std::string &literal);
+	~ScalarConverter();
 
+	static void convert(const std::string &literal);
 };
 #endif
