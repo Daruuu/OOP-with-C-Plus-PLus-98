561116935e5ddab859ed49af92412aa6f168808e
Daruuu <thedaruny16@gmail.com>
2025-10-10 12:26:59 +0200

    add ex02 and done

---
+++ cpp_module_06/ex02/Base.cpp
@@ -0,0 +1,78 @@
+#include "Base.hpp"
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base::~Base()
+{
+}
+
+Base* generate()
+{
+	// std::srand(std::time(NULL));
+	int random = std::rand() % 3;
+
+	switch (random)
+	{
+	case 0:
+		std::cout << "Generated class A" << std::endl;
+		return new A();
+	case 1:
+		std::cout << "Generated class B" << std::endl;
+		return new B();
+	case 2:
+		std::cout << "Generated class C" << std::endl;
+		return new C();
+	default:
+		return NULL;
+	}
+}
+
+void identify(Base* p)
+{
+	if (p == NULL)
+	{
+		std::cout << "Null Pointer" << std::endl;
+		return ;
+	}
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "invalid type" << std::endl;
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		(void) dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		return;
+	}
+	catch (...)
+	{}
+
+	try
+	{
+		(void) dynamic_cast<B&>(p);
+		std::cout << "A" << std::endl;
+		return;
+	}
+	catch (...)
+	{}
+
+	try
+	{
+		(void) dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		return;
+	}
+	catch (...)
+	{}
+	std::cout << "unknow type" << std::endl;
+}
---
+++ cpp_module_06/ex02/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+void randomCases(int maxTestCases)
+{
+	std::cout << GREEN << "===== RANDOM TESTS =====" << RESETT << std::endl;
+
+	for (int i = 1; i <= maxTestCases; ++i)
+	{
+		Base* obj = generate();
+		std::cout << BLUE << "=== Test " << i << " ===" << RESETT << std::endl;
+
+		std::cout << "Pointer version: ";
+		identify(obj);
+
+		std::cout << "Reference version: ";
+		identify(*obj);
+
+		delete obj;
+		std::cout << std::endl;
+	}
+}
+
+void manualTestWithThreeBaseObjects()
+{
+	std::cout << GREEN << "\n===== MANUAL TESTS =====" << RESETT << std::endl;
+
+	A a;
+	B b;
+	C c;
+
+	Base* baseA = &a;
+	Base* baseB = &b;
+	Base* baseC = &c;
+
+	// --- Test A ---
+	std::cout << YELLOW << "\n[TEST A]" << RESETT << std::endl;
+
+	std::cout << "Pointer version: ";
+	identify(baseA);
+
+	std::cout << "Reference version: ";
+	identify(a);
+
+	// --- Test B ---
+	std::cout << YELLOW << "\n[TEST B]" << RESETT << std::endl;
+
+	std::cout << "Pointer version: ";
+	identify(baseB);
+
+	std::cout << "Reference version: ";
+	identify(b);
+
+	// --- Test C ---
+	std::cout << YELLOW << "\n[TEST C]" << RESETT << std::endl;
+
+	std::cout << "Pointer version: ";
+	identify(baseC);
+
+	std::cout << "Reference version: ";
+	identify(c);
+
+	std::cout << "\n" << CYAN << "Addresses for verification:" << RESETT << std::endl;
+	std::cout << "A at " << &a << std::endl;
+	std::cout << "B at " << &b << std::endl;
+	std::cout << "C at " << &c << std::endl;
+
+	std::cout << std::endl;
+}
+
+void edgeCase()
+{
+	std::cout << GREEN << "===== EDGE CASES =====" << RESETT << std::endl;
+
+	std::cout << "Null pointer test: ";
+
+	Base* nullPtr = NULL;
+	identify(nullPtr);
+}
+
+int main()
+{
+	std::srand(static_cast<unsigned int>(std::time(NULL)));
+	randomCases(4);
+	manualTestWithThreeBaseObjects();
+	edgeCase();
+	return 0;
+}
