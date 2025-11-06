8d7d491385ac7b1ed01f40ae337493e8080ecdb8
Daruuu <thedaruny16@gmail.com>
2025-11-06 13:26:55 +0100

    add macros in main.cpp of ex02

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -3,6 +3,15 @@
 #include <list>
 #include <set>
 
+#define RESETT   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
 void test_case_subject()
 {
 	std::cout << GREEN << "***** TEST CASE: SUBJECT *****" << RESETT <<
